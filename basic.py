fcc768b61b2476d1926ae3499f61157c2fb1a735
kpayyam1998 <karuppasamy12041998@gmail.com>
2024-11-09 15:44:04 +0530

    basics

---
+++ basic.py
@@ -0,0 +1,13 @@
+from litellm import completion
+import os
+
+## set ENV variables
+# os.environ["OPENAI_API_KEY"] = "your-api-key"
+
+
+response = completion(
+  model="gpt-3.5-turbo",
+  messages=[{ "content": "Hello, how are you?","role": "user"}]
+)
+
+print(response.choices[0])
